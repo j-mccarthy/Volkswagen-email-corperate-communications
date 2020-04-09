677731151901f051c77f2222536fa5027cb5a659
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-04-09 19:24:11 +0430

    add shadow top right

--- src/helpers/math-minus.js
+++ src/helpers/math-minus.js
@@ -2,7 +2,7 @@ var isNumber = require('is-number');
 
 module.exports = function (a, b) {
   if (isNumber(a) && isNumber(b)) {
-    return Number(a) / Number(b);
+    return Number(a) - Number(b);
   }
   if (typeof a === "string" && typeof b === "string") {
     return a - b;
